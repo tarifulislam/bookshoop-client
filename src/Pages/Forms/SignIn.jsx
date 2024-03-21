1bb533fe8f6704b12b7bd27330caf98672a61dd6
tarifulislam <mdtarifulislam4@gmail.com>
2024-03-22 03:02:50 +0600

    sign in sign up page setup

--- src/Pages/Forms/SignIn.jsx
+++ src/Pages/Forms/SignIn.jsx
@@ -1,8 +1,51 @@
+import { Link,} from "react-router-dom";
+import SocialLogin from "./SocialLogin";
+
+import sidePhoto from "../../assets/img/typeing.jpg"
+import { useRef } from "react";
 
 const SignIn = () => {
+
+    const emailRef = useRef()
+
     return (
-        <div className=" container mx-auto min-h-screen">
-            SignIn
+        <div className="">
+            <div className="container mx-auto min-h-screen py-9 px-[10%]">
+                <div className=" flex flex-col md:flex-row  justify-between items-center">
+                    <div className=" hidden md:block  w-full md:w-2/4 pr-12" >
+                        <img className="  lg:ml-[10%] " src={sidePhoto} alt="" />
+                    </div>
+                    <div className=" w-full md:w-2/4 p-12 rounded-md border">
+                        <h1 className="text-4xl font-bold py-6 text-center">Login now!</h1>
+
+                        <form  className=" space-y-3">
+                            <div className="form-control">
+                                <label className="label font-bold">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="email"  placeholder="email" name="email" ref={emailRef}className="input input-bordered" required />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label font-bold">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <p  className="label-text-alt link link-hover">Forgot password?</p>
+                                </label>
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-secondary">Login</button>
+                            </div>
+                        </form>
+                        <SocialLogin></SocialLogin>
+                        <h4 className=" py-3 font-semibold text-base">create an account?  <Link to='/signup' className=" text-red-600 px-1">Sign Up</Link> </h4>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
