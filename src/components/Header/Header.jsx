2206dfd221a1aca988bfedb4fd11c5692bef1bd0
maker-shihab <frontendmaker99@gmail.com>
2024-03-22 23:00:51 +0600

    add test user name for check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,23 @@
-import {  NavLink } from "react-router-dom";
-import { FaPhoneSquareAlt , FaFacebookF , FaInstagram,  FaBlogger , FaLinkedin , FaSearch,  } from "react-icons/fa";
+import { FaBlogger, FaFacebookF, FaInstagram, FaLinkedin, FaPhoneSquareAlt, FaSearch, } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
+import { NavLink } from "react-router-dom";
 
-import { RiShoppingBagLine } from "react-icons/ri";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { RiShoppingBagLine } from "react-icons/ri";
 
-import logo from '../../assets/img/avater.png'
+import logo from '../../assets/img/avater.png';
+import useAuth from "../../hooks/useAuth";
 
 
 const Header = () => {
-
+    const { userInfo } = useAuth();
+    console.log(userInfo);
     const navLinks = <>
-        <NavLink className={({isActive, isPending})=> isPending ?  "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/">HOME</NavLink>
-        <NavLink className={({isActive, isPending})=> isPending ?  "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/about">ABOUT US</NavLink>
-        <NavLink className={({isActive, isPending})=> isPending ?  "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/books">BOOKS</NavLink>
-        <NavLink className={({isActive, isPending})=> isPending ?  "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/contract">CONTRACT US</NavLink>
-        <NavLink className={({isActive, isPending})=> isPending ?  "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/blog">BLOG</NavLink>
+        <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/">HOME</NavLink>
+        <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/about">ABOUT US</NavLink>
+        <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/books">BOOKS</NavLink>
+        <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/contract">CONTRACT US</NavLink>
+        <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "  pb-1 border-b  border-blue-500  font-semibold" : ""} to="/blog">BLOG</NavLink>
     </>
 
     return (
@@ -24,15 +26,15 @@ const Header = () => {
             <div className="py-2  px-[2%]   hidden md:block">
                 <div className=" container mx-auto flex justify-between items-center font-semibold py-2 border-b">
                     <div className="flex items-center">
-                     <span><FaPhoneSquareAlt /></span>
-                     <h2 className="ml-2">+015656582</h2>
-                    </div> 
+                        <span><FaPhoneSquareAlt /></span>
+                        <h2 className="ml-2">+015656582</h2>
+                    </div>
                     <div className=" flex items-center space-x-4">
                         <span><FaFacebookF /></span>
                         <span><FaInstagram /></span>
                         <span><FaBlogger /></span>
                         <span><FaLinkedin /></span>
-                    </div> 
+                    </div>
                 </div>
 
                 <div className="">
@@ -41,10 +43,11 @@ const Header = () => {
                             <div className="w-1/12">
                                 {/* <div className=" bg-green-500 w-14 h-14 rounded-full"></div> */}
                                 <img className=" w-12 h-12 rounded-full" src={logo} alt="" />
+                                <p>{userInfo.userName}</p>
                             </div>
                             <div className="w-7/12 ">
                                 <div className="w-3/4 relative">
-                                    <input className=" bg-[#f5f1f1a4] outline-0 border-none px-4 py-2 w-full rounded-full"  type="text" placeholder="Search books"/>
+                                    <input className=" bg-[#f5f1f1a4] outline-0 border-none px-4 py-2 w-full rounded-full" type="text" placeholder="Search books" />
                                     <span className=" absolute right-4 top-3 text-[#6a6a6a]"><FaSearch /></span>
                                 </div>
                             </div>
@@ -67,26 +70,26 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-                    
+
             <div className="bg-[#173F5F]">
                 <div className="navbar md:text-white  container mx-auto  px-0">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 w-96  leading-7 h-48">
+                                {navLinks}
+                            </ul>
+                        </div>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 w-96  leading-7 h-48">
-                        {navLinks}
-                    </ul>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 space-x-9">
+                            {navLinks}
+                        </ul>
                     </div>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 space-x-9">
-                        {navLinks}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                </div>
+                    <div className="navbar-end">
+                    </div>
                 </div>
             </div>
         </div>
