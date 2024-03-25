3a8d5c8dafdd88697a6d5bbca34b5a09adfa8d82
maker-shihab <frontendmaker99@gmail.com>
2024-03-26 01:02:07 +0600

    update some issues

--- src/Pages/Forms/SignIn.jsx
+++ src/Pages/Forms/SignIn.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import authApi from "../../api/authApi";
 import sidePhoto from "../../assets/img/typeing.jpg";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 import useAuth from "../../hooks/useAuth";
 
 const SignIn = () => {
@@ -20,7 +22,7 @@ const SignIn = () => {
             const response = await authApi.login({ identifier, password }, {
                 withCredentials: true
             });
-            
+
             if (response?.data?.success) {
                 handleLoginSuccess({ accessToken: response?.data?.data.accessToken });
                 navigate('/');
@@ -34,47 +36,51 @@ const SignIn = () => {
     }
 
     return (
-        <div className="">
-             <Toaster
-                position="bottom-right"
-                reverseOrder={false}
-            />
-            <div className="container mx-auto min-h-screen py-9 px-[10%]">
-                <div className=" flex flex-col md:flex-row  justify-between items-center">
-                    <div className=" hidden md:block  w-full md:w-2/4 pr-12" >
-                        <img className="  lg:ml-[10%] " src={sidePhoto} alt="thumb" />
-                    </div>
-                    <div className=" w-full md:w-2/4 p-12 rounded-md border">
-                        <h1 className="text-4xl font-bold py-6 text-center">Login now!</h1>
+        <div>
+            <Header />
+            <div className="">
+                <Toaster
+                    position="bottom-right"
+                    reverseOrder={false}
+                />
+                <div className="container mx-auto min-h-screen py-9 px-[10%]">
+                    <div className=" flex flex-col md:flex-row  justify-between items-center">
+                        <div className=" hidden md:block  w-full md:w-2/4 pr-12" >
+                            <img className="  lg:ml-[10%] " src={sidePhoto} alt="thumb" />
+                        </div>
+                        <div className=" w-full md:w-2/4 p-12 rounded-md border">
+                            <h1 className="text-4xl font-bold py-6 text-center">Login now!</h1>
 
-                        <form className="space-y-3" onSubmit={handleSubmit}>
-                            <div className="form-control">
-                                <label className="label font-bold">
-                                    <span className="label-text">Email or UserName:</span>
-                                </label>
-                                <input type="text" placeholder="Email or UserName" name="text" className="input input-bordered" required onChange={(e) => setIdentifier(e.target.value)} />
-                            </div>
+                            <form className="space-y-3" onSubmit={handleSubmit}>
+                                <div className="form-control">
+                                    <label className="label font-bold">
+                                        <span className="label-text">Email or UserName:</span>
+                                    </label>
+                                    <input type="text" placeholder="Email or UserName" name="text" className="input input-bordered" required onChange={(e) => setIdentifier(e.target.value)} />
+                                </div>
 
-                            <div className="form-control">
-                                <label className="label font-bold">
-                                    <span className="label-text">Password:</span>
-                                </label>
-                                <input type="password" placeholder="password" name="password" className="input input-bordered" required onChange={(e) => setPassword(e.target.value)} />
-                                <label className="label">
-                                    <p className="label-text-alt link link-hover">Forgot password?</p>
-                                </label>
-                            </div>
+                                <div className="form-control">
+                                    <label className="label font-bold">
+                                        <span className="label-text">Password:</span>
+                                    </label>
+                                    <input type="password" placeholder="password" name="password" className="input input-bordered" required onChange={(e) => setPassword(e.target.value)} />
+                                    <label className="label">
+                                        <p className="label-text-alt link link-hover">Forgot password?</p>
+                                    </label>
+                                </div>
 
-                            <div className="form-control mt-6">
-                                <button type="submit" className="btn btn-success text-white">Login</button>
-                            </div>
-                        </form>
-                        <SocialLogin></SocialLogin>
-                        <h4 className=" py-3 font-semibold text-base">create an account?  <Link to='/signup' className=" text-red-600 px-1">Sign Up</Link> </h4>
-                        
+                                <div className="form-control mt-6">
+                                    <button type="submit" className="btn btn-success text-white">Login</button>
+                                </div>
+                            </form>
+                            <SocialLogin></SocialLogin>
+                            <h4 className=" py-3 font-semibold text-base">create an account?  <Link to='/signup' className=" text-red-600 px-1">Sign Up</Link> </h4>
+
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,37 @@
 import useAuth from "../../hooks/useAuth";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Profile = () => {
   const { userDetails } = useAuth();
   return (
-    <div className="container mx-auto mt-8">
-      <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-        <div className="px-6 py-8">
-          <div className="flex justify-center">
-            <img
-              className="w-32 h-32 rounded-full object-cover"
-              src={userDetails?.information?.profileImage}
-              alt={userDetails?.name?.firstName}
-            />
-          </div>
-          <h2 className="mt-4 text-3xl font-semibold text-center capitalize">{userDetails?.name?.firstName}</h2>
-          <p className="mt-2 text-sm text-gray-600 text-center">{userDetails?.email}</p>
-          <div className="mt-2">
-            <p className="mt-2 text-gray-600 text-center font-semibold">{userDetails?.information?.bio}</p>
-          </div>
-          <div className="mt-2">
-            <h3 className="text-xl font-semibold">{userDetails?.city}</h3>
-            <p className="mt-2 text-gray-600 text-center">
-              {userDetails?.information?.address}, {userDetails?.information?.city}, {userDetails?.information?.state}, {userDetails?.information?.zipCode}
-            </p>
+    <div className="">
+      <Header />
+      <div className="container mx-auto mt-8 py-20">
+        <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+          <div className="px-6 py-8">
+            <div className="flex justify-center">
+              <img
+                className="w-32 h-32 rounded-full object-cover"
+                src={userDetails?.information?.profileImage}
+                alt={userDetails?.name?.firstName}
+              />
+            </div>
+            <h2 className="mt-4 text-3xl font-semibold text-center capitalize">{userDetails?.name?.firstName}</h2>
+            <p className="mt-2 text-sm text-gray-600 text-center">{userDetails?.email}</p>
+            <div className="mt-2">
+              <p className="mt-2 text-gray-600 text-center font-semibold">{userDetails?.information?.bio}</p>
+            </div>
+            <div className="mt-2">
+              <h3 className="text-xl font-semibold">{userDetails?.city}</h3>
+              <p className="mt-2 text-gray-600 text-center">
+                {userDetails?.information?.address}, {userDetails?.information?.city}, {userDetails?.information?.state}, {userDetails?.information?.zipCode}
+              </p>
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
