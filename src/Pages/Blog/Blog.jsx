485bf462749e6da7353523c7c71bda9f4f482387
tarifulislam <mdtarifulislam4@gmail.com>
2024-03-26 23:45:21 +0600

    add blog page

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -6,7 +6,106 @@ const Blog = () => {
         <div>
             <Header></Header>
             <div className=" container mx-auto min-h-screen">
-                Blog
+               <div className=" flex py-9">
+                    <div className=" w-8/12">
+                    <div className="card card-compact w-full bg-base-100 shadow-md">
+                    <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="px-9 space-y-2 py-6">
+                        <p className="  text-xl text-[#6a6a6a]">company</p>
+                        <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                    </div>
+                    </div>
+
+                    </div>
+                    <div className=" w-4/12">
+                        <div className=" ml-[5%]" >
+                            <h2 className=" text-2xl font-semibold">Book Details</h2>
+                            <div className="ml-[5%] space-y-6 py-4">
+
+                                <div className="border rounded p-2 space-y-1">
+                                    <p className=" text-cyan-500">How to</p>
+                                    <h3 className=" text-xl text-[#6a6a6a]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus.</h3>
+                                </div>
+
+                                <div className="border rounded p-2 space-y-1">
+                                    <p className=" text-cyan-500">How to</p>
+                                    <h3 className=" text-xl text-[#6a6a6a]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus.</h3>
+                                </div>
+
+                                <div className="border rounded p-2 space-y-1">
+                                    <p className=" text-cyan-500">How to</p>
+                                    <h3 className=" text-xl text-[#6a6a6a]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus consectetur adipisicing elit. Possimus.</h3>
+                                </div>
+
+                                <div className="border rounded p-2 space-y-1">
+                                    <p className=" text-cyan-500">How to</p>
+                                    <h3 className=" text-xl text-[#6a6a6a]">Lorem ipsum dolor sit  dolor sit amet consectetur adipisicin amet consectetur adipisicing elit. Possimus.</h3>
+                                </div>
+
+                                <div className="border rounded p-2 space-y-1">
+                                    <p className=" text-cyan-500">How to</p>
+                                    <h3 className=" text-xl text-[#6a6a6a]">Lorem ipsum dolor   dolor sit amet consectetur adipisicin sit amet consectetur adipisicing elit. Possimus.</h3>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+               </div>
+
+               <div className=" grid grid-cols-3 gap-6">
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+                <div className="card card-compact w-full bg-base-100  shadow-md">
+                        <figure><img className=" w-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="px-9 space-y-2 py-6">
+                            <p className="  text-xl text-[#6a6a6a]">company</p>
+                            <h2 className=" text-2xl font-semibold  pr-[10%] ">Lorem, ipsum dolor sit amet consectetur adipisicing. amet consectetur adipisicing. amet consectetur adipisicing.</h2>
+                        </div>
+                    </div>
+
+
+
+
+               </div>
             </div>
             <Footer></Footer>
         </div>
